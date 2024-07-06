524f867b16a25a9355bf1f74b035753e8bffad3b
Raymond8837 <raymond.tszhintang@gmail.com>
2024-07-06 13:12:46 +1000

    completed

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "c3adcc7745e06e1b15727293e2136863"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d4ef13faef644890a244bab03ec5f940"
